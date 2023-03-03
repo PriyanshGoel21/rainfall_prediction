186bd9dcdcaa87ecb2844adf876f45479fcfdc18
Priyansh Goel <priyanshgoel05@gmail.com>
2023-03-04 01:23:25 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn import metrics
-from sklearn.ensemble import RandomForestRegressor
+# import pandas as pd
+# import numpy as np
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LinearRegression
+# from sklearn import metrics
+# from sklearn.ensemble import RandomForestRegressor
 import pickle
 st.set_page_config(
     page_title="Rainfall Prediction using RandomForest",
